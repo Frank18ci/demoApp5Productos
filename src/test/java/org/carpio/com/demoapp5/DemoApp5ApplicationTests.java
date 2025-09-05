53d4bf2b6ff3525f2abd43eb46612c88d3698cca
Frank <fcarpio113@gmail.com>
2025-09-04 21:12:43 -0500

    first commit

---
+++ src/test/java/org/carpio/com/demoapp5/DemoApp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carpio.com.demoapp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApp5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
